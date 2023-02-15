c8fb92425bc8f4fa04a93e0e1f9224b694a988ef
Nazila <azabdaftar@outlook.com>
2023-02-15 19:11:46 +0100

    EX2 finished

--- Nazila/EX-2/js/script.js
+++ Nazila/EX-2/js/script.js
@@ -18,3 +18,25 @@ var _loop_1 = function (i) {
 for (var i = 0; i < 10; i++) {
     _loop_1(i);
 }
+var printArrayObjs = [
+    { fName: "Nazila", lName: "Azabdaftar" },
+];
+var resultPrint = document.getElementById("resultFunction");
+var _loop_2 = function (value) {
+    var _loop_3 = function (i) {
+        // resultPrint.innerHTML += printName(value.lName,i);
+        var int = setInterval(function () {
+            resultPrint.innerHTML += printName(value.lName, i);
+        }, 5000);
+    };
+    for (var i = 0; i < 10; i++) {
+        _loop_3(i);
+    }
+};
+for (var _a = 0, printArrayObjs_1 = printArrayObjs; _a < printArrayObjs_1.length; _a++) {
+    var value = printArrayObjs_1[_a];
+    _loop_2(value);
+}
+function printName(lastName, no) {
+    return "".concat(no, " - ").concat(lastName, "<br>");
+}
--- Nazila/EX-2/js/script.ts
+++ Nazila/EX-2/js/script.ts
@@ -9,15 +9,43 @@ for (let arrayValue of arrayObj) {
     `;
 }
 
-let neuArrayObjs: Array<{ fName: String, lName: string }> = [
+let neuArrayObjs: Array<{ fName: String, lName: String }> = [
     { fName: "Nazila", lName: "Azabdaftar" },];
 
 const resultRepeat = document.getElementById("resultFullName") as HTMLElement;
 
 for (let i = 0; i < 10; i++) {
     neuArrayObjs.forEach(arrayObj => {
-        resultRepeat.innerHTML +=`
+        resultRepeat.innerHTML += `
         ${i}-${arrayObj.fName} ${arrayObj.lName}<br>
         `;
     });
 }
+
+
+let printArrayObjs: Array<{ fName: String, lName: String }> = [
+    { fName: "Nazila", lName: "Azabdaftar" },];
+
+
+
+const resultPrint = document.getElementById("resultFunction") as HTMLElement;
+
+for (let value of printArrayObjs) {
+    for (let i = 0; i < 10; i++) {
+
+        // resultPrint.innerHTML += printName(value.lName,i);
+       let int: number=setInterval(function(){
+        resultPrint.innerHTML+= printName(value.lName,i);
+       },5000);
+      
+       
+    }
+
+}
+
+function printName(lastName: String, no: number) {
+
+    return `${no} - ${lastName}<br>`;
+
+}
+
